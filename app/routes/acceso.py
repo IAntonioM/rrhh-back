1932e48e28bbe3f4e89c1930c50c9148a5744761
IAntonioM <isaiasmayhuaym@gmail.com>
2025-02-16 18:48:03 -0500

    modulo-seguridad

--- app/routes/acceso.py
+++ app/routes/acceso.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from ..models.menu import MenuModel
+from ..models.acceso import AccesoModel
 from ..utils.error_handlers import handle_response
 import re
 
-menu_bp = Blueprint('menu', __name__)
+acceso_bp = Blueprint('acceso', __name__)
 
 def handle_sql_error(e):
     """Handles SQL errors by extracting the error message."""
@@ -12,107 +12,105 @@ def handle_sql_error(e):
     matches = re.search(r'\[SQL Server\](.*?)(?:\(|\[|$)', error_msg)
     return matches.group(1).strip() if matches else 'Error en la operación'
 
-@menu_bp.route('/create', methods=['POST'])
+@acceso_bp.route('/create', methods=['POST'])
 @jwt_required()
 @handle_response
-def create_menu():
+def create_acceso():
     current_user = get_jwt_identity()
     if not current_user:
         return jsonify({'success': False, 'message': 'Usuario no encontrado'}), 404
 
     data = request.get_json()
-    success, message = MenuModel.create_menu(data, current_user, request.remote_addr)
+    success, message = AccesoModel.create_acceso(data, current_user, request.remote_addr)
     return jsonify({
         'success': success,
         'message': message
     }), 201 if success else 409
 
-@menu_bp.route('/update/<int:id>', methods=['PUT'])
+@acceso_bp.route('/update/<int:id>', methods=['PUT'])
 @jwt_required()
 @handle_response
-def update_menu(id):
+def update_acceso(id):
     current_user = get_jwt_identity()
     if not current_user:
         return jsonify({'success': False, 'message': 'Usuario no encontrado'}), 404
     
     data = request.get_json()
-    success, message = MenuModel.update_menu(data, current_user, request.remote_addr)
+    success, message = AccesoModel.update_acceso(data, current_user, request.remote_addr)
     return jsonify({
         'success': success,
         'message': message
     }), 200 if success else 409
 
-@menu_bp.route('/filtrar', methods=['GET'])
+@acceso_bp.route('/filtrar', methods=['GET'])
 @jwt_required()
 @handle_response(include_data=True)
-def get_menus():
+def get_accesos():
     # Obtener filtros y paginación desde los parámetros de consulta
     filtros = {
         'nombre': request.args.get('nombre') or None,
+        'objeto_id': request.args.get('objeto_id') or None,
+        'tipo': request.args.get('tipo') or None,
+        'estado': request.args.get('estado') or None,
     }
     
     current_page = int(request.args.get('current_page', 1))
     per_page = int(request.args.get('per_page', 10))
     
-    # Obtener lista de menús filtrados
-    menus_list = MenuModel.get_menus_filter(filtros, current_page, per_page)
+    # Obtener lista de accesos filtrados
+    accesos_list = AccesoModel.get_accesos_filter(filtros, current_page, per_page)
     return jsonify({
         'success': True,
-        'data': menus_list
+        'data': accesos_list
     }), 200
 
-
-@menu_bp.route('/<int:menu_id>', methods=['GET'])
+@acceso_bp.route('/<int:acceso_id>', methods=['GET'])
 @jwt_required()
 @handle_response
-def get_menu(menu_id):
-    menu = MenuModel.get_menu(menu_id)
-    if not menu:
-        return jsonify({'success': False, 'message': 'Menu no encontrado'}), 404
+def get_acceso(acceso_id):
+    acceso = AccesoModel.get_acceso(acceso_id)
+    if not acceso:
+        return jsonify({'success': False, 'message': 'Acceso no encontrado'}), 404
     return jsonify({
         'success': True,
-        'data': menu
+        'data': acceso
     }), 200
 
-@menu_bp.route('/delete/<int:menu_id>', methods=['DELETE'])
+@acceso_bp.route('/delete/<int:acceso_id>', methods=['DELETE'])
 @jwt_required()
 @handle_response
-def delete_menu(menu_id):
+def delete_acceso(acceso_id):
     current_user = get_jwt_identity()
     if not current_user:
         return jsonify({'success': False, 'message': 'Usuario no encontrado'}), 404
 
-    success, message = MenuModel.delete_menu(menu_id, current_user, request.remote_addr)
+    success, message = AccesoModel.delete_acceso(acceso_id, current_user, request.remote_addr)
     return jsonify({
         'success': success,
         'message': message
     }), 200 if success else 409
 
-
-@menu_bp.route('/list', methods=['GET'])
+@acceso_bp.route('/list', methods=['GET'])
 @jwt_required()
 @handle_response(include_data=True)
-def get_menus_list():
-    filtros = {
-        'padre_id': request.args.get('padre_id') or None,
-    }
-    # Optionally accept query parameters to filter menus (e.g., active, by parent_id, etc.)
-    menus = MenuModel.get_menus_list_complete(filtros)
+def get_accesos_list():
+    # Optionally accept query parameters to filter accesos (e.g., active, by tipo, etc.)
+    accesos = AccesoModel.get_accesos_list_complete()
     return jsonify({
         'success': True,
-        'data': menus
+        'data': accesos
     }), 200
-    
-@menu_bp.route('/<int:id>/<int:estado>/estado', methods=['PATCH'])
+
+@acceso_bp.route('/<int:id>/<int:estado>/estado', methods=['PATCH'])
 @jwt_required()
 @handle_response
-def update_menu_status(id, estado):
+def update_acceso_status(id, estado):
     current_user = get_jwt_identity()
     if not current_user:
         return jsonify({'success': False, 'message': 'Usuario no encontrado'}), 404
     
-    # Llamamos a la función estática para cambiar el estado del menú
-    success, message = MenuModel.change_menu_status(id, estado, current_user, request.remote_addr)
+    # Llamamos a la función estática para cambiar el estado del acceso
+    success, message = AccesoModel.change_acceso_status(id, estado, current_user, request.remote_addr)
     
     return jsonify({
         'success': success,
